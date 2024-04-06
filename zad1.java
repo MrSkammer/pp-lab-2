91ef73be009e553c962884a9ff48105fbe2168c5
Kamil <mrkamkam2053@gmail.com>
2024-04-06 14:39:29 +0200

    zad2

--- zad1.java
+++ zad1.java
@@ -1,14 +1,51 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class zad1 {
     public static void main(String[] ars){
-        double math, science, history, english, chemistry;
-        math = 5;
-        science = 3.5;
-        history = 3;
-        english = 4;
-        chemistry = 2;
+        Scanner scanner = new Scanner(System.in);
+        
+        double math, physics, history, english, chemistry;
+        double suma = 0;
+        
+        System.out.println("Podaj ocene z matematyki:");
+        math = scanner.nextDouble();
 
-        double average = (math + science + history + english + chemistry)/5;
-        System.out.println(average);
-    }
+        System.out.println("Podaj ocene z fizyki:");
+        physics = scanner.nextDouble();
+
+        System.out.println("Podaj ocene z historii:");
+        history = scanner.nextDouble();
+
+        System.out.println("Podaj ocene z angielskiego:");
+        english = scanner.nextDouble();
+
+        System.out.println("Podaj ocene z chemii:");
+        chemistry = scanner.nextDouble();
+
+        double[] grades = {math, history, english, chemistry, physics};
+        
+        
+        System.out.println("Czy chcesz dodać kolejną ocene? Y/N");
+        scanner.nextLine();
+        String answer = scanner.nextLine(); 
+        while (answer.contains("Y")) {
+            System.out.println("Podaj kolejną ocenę:");
+            double newGrade = scanner.nextDouble();
+            grades = Arrays.copyOf(grades, grades.length + 1);
+            grades[grades.length - 1] = newGrade;
+            System.out.println(Arrays.toString(grades));
+
+            System.out.println("Czy chcesz dodać kolejną ocene? Y/N");
+            scanner.nextLine();
+            answer = scanner.nextLine(); 
+        }
+        
+        for (int i = 0; i < grades.length; i++) {
+            suma += grades[i];
+        }
+        System.out.println(suma);
+        System.out.println(suma/grades.length);
+    }    
 
 }
