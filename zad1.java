671409f962104df639c972a973784a77d29f0ebc
Kamil <mrkamkam2053@gmail.com>
2024-04-06 13:18:45 +0200

    something

---
+++ zad1.java
@@ -0,0 +1,14 @@
+public class zad1 {
+    public static void main(String[] ars){
+        double math, science, history, english, chemistry;
+        math = 5;
+        science = 3.5;
+        history = 3;
+        english = 4;
+        chemistry = 2;
+
+        double average = (math + science + history + english + chemistry)/5;
+        System.out.println(average);
+    }
+
+}
