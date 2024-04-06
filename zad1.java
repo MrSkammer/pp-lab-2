8e6b7bc325c5430493375aca35a49cf9d9e6e1e0
Kamil <mrkamkam2053@gmail.com>
2024-04-06 13:31:17 +0200

    zad 1 complete

--- zad1.java
+++ zad1.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class zad1 {
     public static void main(String[] ars){
-        double math, science, history, english, chemistry;
-        math = 5;
-        science = 3.5;
-        history = 3;
-        english = 4;
-        chemistry = 2;
+        Scanner scanner = new Scanner(System.in);
+        
+        double math, physics, history, english, chemistry;
+        
+        System.out.println("Podaj ocene z matematyki:");
+        math = scanner.nextDouble();
 
-        double average = (math + science + history + english + chemistry)/5;
+        System.out.println("Podaj ocene z fizyki:");
+        physics = scanner.nextDouble();
+
+        System.out.println("Podaj ocene z historii:");
+        history = scanner.nextDouble();
+
+        System.out.println("Podaj ocene z angielskiego:");
+        english = scanner.nextDouble();
+
+        System.out.println("Podaj ocene z chemii:");
+        chemistry = scanner.nextDouble();
+
+        double average = (math + physics + history + english + chemistry)/5;
         System.out.println(average);
-    }
-
+    }    
 }
